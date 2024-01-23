d63b3661271a07917b967e67e5a3174dbf27ad6d
Shushant Giri <118593259+shushantgiri@users.noreply.github.com>
2024-01-23 18:20:44 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -139,3 +139,16 @@
     });
 
 })(jQuery);
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 300) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
